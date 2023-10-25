7e1bb7964fadb98f7f44f049292573087e1783a5
aijamalabdybapova <isip_a.e.abdybapova@mpt.ru>
2023-10-25 12:19:08 +0300

    доп к 4 прак
    доп к 4 прак

---
+++ доп _к _4.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualBasic;
+using System.Collections.Generic;
+
+namespace Zametka
+{
+    public class Zametka
+    {
+        public int Day;
+        public int Month = 10;
+        public string opisanie;
+        public string name;
+        public int Number;
+        public DateTime CreationTime;
+        public static List<Zametka> zametki = new List<Zametka>();
+    }
+}
